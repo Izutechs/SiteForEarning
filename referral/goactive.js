9c4c0e90980dfa8f0b7f144ce3ba702b1fe121fc
Izutechs <55264330+Izutechs@users.noreply.github.com>
2020-09-17 00:09:58 +0100

    Create goactive.js

---
+++ referral/goactive.js
@@ -0,0 +1,30 @@
+
+//     let checkActtop=localStorage.getItem("hveacttopline");
+//     let actreef=document.getElementById("actreef");
+    
+//     const hveacttopline="hveacttopline";
+//     let num=1;
+//     if(actreef){
+//         actreef.addEventListener("click", saveRefact);
+//         }
+//     function warnSaved(){
+//         alert("Your referrer has already activated your referral reward!");
+//     }
+//     function saveRefact(){
+//         if(checkActtop==null){
+//             localStorage.setItem(hveacttopline, num);
+//         }
+//         else{
+        
+//         warnSaved();
+//             event.preventDefault();
+//     }
+// }
+  
+
+// //     actreef.removeEventListener("click", saveRefact);
+// //     actreef.addEventListener("click", warnSaved);
+// //     event.preventDefault();
+// // }
+
+    
